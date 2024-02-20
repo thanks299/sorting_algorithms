ad51b411ebaaf2bb145a4abb8c07620add45e008
thanks299 <thanksagbeble@gmail.com>
2024-02-20 15:04:02 -0800

    sorting algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,8 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble sort.
+ *bubble_sort - Sorts an array of integers
+ *in ascending order using Bubble sort.
  * @array: Array being sorted.
  * @size: Size of the array.
  *
