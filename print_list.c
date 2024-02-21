837fd9d8d18fbfa7f67765e69034f437f748ec85
thanks299 <thanksagbeble@gmail.com>
2024-02-20 16:23:08 -0800

    sorting algorithm

--- print_list.c
+++ print_list.c
@@ -14,36 +14,36 @@ return;
 current = *list;
 next = current->next;
 /* The sorted list starts with the first node */
-    sorted = current;
-    sorted_prev = NULL;
-
-    while (next != NULL) {
-        /* If the current node is smaller than the previous node in the sorted list,
-         * we need to insert it before that node */
-        if (current->n < sorted_prev->n) {
-            /* Remove the current node from the list */
-            if (current->prev != NULL)
-                current->prev->next = current->next;
-            if (current->next != NULL)
-                current->next->prev = current->prev;
-            if (sorted_prev->prev != NULL)
-                sorted_prev->prev->next = current;
-            if (sorted == current)
-                sorted = current->next;
-            current->prev = sorted_prev->prev;
-            current->next = sorted_prev;
-            if (sorted_prev->prev != NULL)
-                sorted_prev->prev = current;
-            else
-                *list = current;
-            sorted_prev = current;
-        } else {
-            /* Otherwise, we just need to update the sorted_prev pointer */
-            sorted_prev = next;
-        }
-
-        /* Move to the next pair of nodes */
-        current = next;
-        next = current->next;
-    }
+sorted = current;
+sorted_prev = NULL;
+while (next != NULL) {
+/* If the current node is smaller than 
+* the previous node in the sorted list,
+* we need to insert it before that node */
+if (current->n < sorted_prev->n) {
+/* Remove the current node from the list */
+if (current->prev != NULL)
+current->prev->next = current->next;
+if (current->next != NULL)
+current->next->prev = current->prev;
+if (sorted_prev->prev != NULL)
+sorted_prev->prev->next = current;
+if (sorted == current)
+sorted = current->next;
+current->prev = sorted_prev->prev;
+current->next = sorted_prev;
+if (sorted_prev->prev != NULL)
+sorted_prev->prev = current;
+else
+*list = current;
+sorted_prev = current;
+} else {
+/* Otherwise, we just need to update 
+* the sorted_prev pointer */
+sorted_prev = next;
+}
+/* Move to the next pair of nodes */
+current = next;
+next = current->next;
+}
 }
