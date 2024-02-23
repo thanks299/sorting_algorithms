0c8ef10dd831c001a552714764226200cbc88dbc
thanks <thanksagbeble@gmail.com>
2024-02-23 11:21:04 -0800

    sorting algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -80,9 +80,7 @@ int main(void)
 	return (0);
 }
 
-
 #include "sort.h"
-
 
 /**
  * _swap - Swaps two integers
@@ -94,4 +92,15 @@ void _swap(int *a, int *b)
 	int temp = *a;
 	*a = *b;
 	*b = temp;
+}
+
+int main(void)
+{
+	int array[] = {9, 7, 5, 11, 12, 2, 14, 3, 10, 6};
+	size_t size = sizeof(array) / sizeof(array[0]);
+
+	quick_sort(array, size);
+	print_array(array, size);
+
+	return (0);
 }
