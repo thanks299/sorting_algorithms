50f423178b3a0302a3008731443ede074319fc9d
thanks <thanksagbeble@gmail.com>
2024-02-23 10:21:54 -0800

    sorting files

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,23 +1,24 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
  * selection_sort - Sorts an array of integers in ascending order
  * using the Selection sort algorithm
  * @array: The array to be sorted
- * @size: Number of elements in @array
+ * @array_size: Number of elements in @array
  */
-void selection_sort(int *array, int size)
+void selection_sort(int *array, size_t array_size)
 {
 size_t i, j, min_index;
 int temp;
 
-if (array == NULL || size < 2)
+if (array == NULL || array_size < 2)
 return;
-for (i = 0; i < size - 1; i++)
+
+for (i = 0; i < array_size - 1; i++)
 {
 min_index = i;
-for (j = i + 1; j < size; j++)
+
+for (j = i + 1; j < array_size; j++)
 {
 if (array[j] < array[min_index])
 min_index = j;
@@ -27,7 +28,8 @@ if (min_index != i)
 temp = array[i];
 array[i] = array[min_index];
 array[min_index] = temp;
-print_array(array, size);
+/* Print the array after each swap */
+print_array(array, array_size);
 }
 }
 }
