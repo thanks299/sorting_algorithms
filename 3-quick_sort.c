43fb9d2fc892a00dcc239b7ed08ec1f4b62243ee
thanks299 <139892876+thanks299@users.noreply.github.com>
2024-02-23 19:57:25 +0100

    sorting algorithm

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -79,3 +79,19 @@ int main(void)
 
 	return (0);
 }
+
+
+#include "sort.h"
+
+
+/**
+ * _swap - Swaps two integers
+ * @a: Pointer to the first integer
+ * @b: Pointer to the second integer
+ */
+void _swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
